b4fd5729aca50d17f14f7eb5ab0975e50b588112
Florian Schrag <florian@schrag.org>
2014-06-03 11:17:15 +0200

    initial display

---
+++ RaceDisplay/src/java/com/zuehlke/race/display/Main.java
@@ -0,0 +1,10 @@
+package com.zuehlke.race.display;
+
+import processing.core.PApplet;
+
+public class Main {
+
+    public static void main(String[] args) {
+        PApplet.main(new String[]{"--present","com.zuehlke.race.display.GForceDisplay"});
+    }
+}
