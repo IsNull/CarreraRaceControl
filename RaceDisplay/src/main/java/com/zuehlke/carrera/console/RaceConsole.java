d3df749e3947372dc35c451e4cab15887c2b691e
Pascal Büttiker <pacal.buettiker@zuehlke.com>
2014-09-29 14:09:34 +0200

    race console use separated API

--- RaceDisplay/src/main/java/com/zuehlke/carrera/console/RaceConsole.java
+++ RaceDisplay/src/main/java/com/zuehlke/carrera/console/RaceConsole.java
@@ -1,11 +1,10 @@
 package com.zuehlke.carrera.console;
 
-import com.zuehlke.carrera.model.WebSocket;
+import com.zuehlke.carrera.model.racetrack.RaceTrackAPI;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.URI;
 import java.net.URISyntaxException;
 
 /**
@@ -14,13 +13,8 @@ import java.net.URISyntaxException;
 public class RaceConsole {
 
     public static void main ( String [] args ) throws URISyntaxException, IOException {
-        if ( args.length != 0 ) {
-            System.exit(0);
-        }
 
-        WebSocket ws = new WebSocket(new URI(System.getProperty("url","ws://127.0.0.1:9000")));
-
-        ws.connect();
+        RaceTrackAPI raceTrackAPI = new RaceTrackAPI();
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -34,16 +28,14 @@ public class RaceConsole {
                     System.out.println("Ending race console...");
                     continuing = false;
                 } else {
-                    String command = "C1: " + cmd;
-                    System.out.println("Sending '" + command + "' to race control!");
-                    ws.send(command);
+                    raceTrackAPI.setCarSpeed(Integer.parseInt(cmd));
                 }
 
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
         }
-        ws.close();
+        raceTrackAPI.disconnectSpeedControl();
         System.out.println("Race-Console has been ended.");
 
 
